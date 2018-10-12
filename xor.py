a601555c4d75050d98f320331ef85f92f5c3d5f7
jmanfredi <juanjmanfredi@gmail.com>
2018-10-12 09:55:37 -0400

    Adding an example

---
+++ xor.py
@@ -0,0 +1,36 @@
+"""
+The canonical example of a function that can't be
+learned with a simple model is XOR
+"""
+import numpy as np
+
+from juannet.train import train
+from juannet.nn import NeuralNet
+from juannet.layers import Linear, Tanh
+
+inputs = np.array([
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1]
+    ])
+
+#I am not sure the outputs below make sense, but Joel
+#wants the "true" output to be represented by [0,1]
+#and the "false" by [1,0]
+targets = np.array([
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [1, 0]
+    ])
+
+net = NeuralNet([
+    Linear(input_size=2, output_size=2)
+    ])
+
+train(net, inputs, targets)
+
+for x,y in zip(inputs, targets):
+    predicted = net.forward(x)
+    print(x, predicted, y)
