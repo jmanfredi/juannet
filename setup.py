c3ae4cebecdd65939dc85f192517b9679b9f79bb
jmanfredi <juanjmanfredi@gmail.com>
2018-10-16 17:53:18 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='juannet',
+    version='0.1',
+    packages=['juannet',],
+    install_requires=['numpy'],
+    author=['Juan Manfredi'],
+    author_email='juanjmanfredi@gmail.com',
+    url='https://github.com/jmanfredi/juannet',
+    long_description=open('README.txt').read(),
+    )
